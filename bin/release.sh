392440413f62605a760555b11a68a18553534315
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-11-17 12:28:37 +0300

    Update bin/release.sh script
    * Delete svn directory on each time
    * Copy banners and icons

--- bin/release.sh
+++ bin/release.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+rm -rf svn
+
 svn co https://plugins.svn.wordpress.org/widget-to-show-posts-in-current-category/ svn
-rm -rf svn/assets/*
-rm -rf svn/trunk/*
 
 cp -Rv assets/screenshots/* svn/assets/
+cp -Rv assets/banners/* svn/assets/
+cp -Rv assets/icons/* svn/assets/
 cp -Rv LICENSE readme.txt widget-to-display-posts-from-current-category.php svn/trunk/
